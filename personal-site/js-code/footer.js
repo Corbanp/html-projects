aa2c932a63925a859c2af2f1c39adb03f8108767
corbanp <corbanp@icloud.com>
2024-05-28 23:11:04 -0700

    trying to fix things up

--- personal-site/js-code/footer.js
+++
@@ -1,7 +0,0 @@
-// footerLoader.js
-function includeFooter() {
-    var footer = document.createElement('footer');
-    footer.innerHTML = '<object type="text/html" data="footer.html" ></object>';
-    document.body.appendChild(footer);
-}
-includeFooter();
